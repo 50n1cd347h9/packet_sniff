f981db97a1dbeefaa53b982d2ced244fd11dd2e8
50n1cd347h9 <8yzuie9g@duck.com>
2024-06-06 21:39:06 +0900

    receive packets

--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,33 @@ const c = @cImport({
 });
 const pcap_t: type = c.pcap_t;
 
+fn callback(user: [*c]u8, pkthdr: [*c]const c.pcap_pkthdr, packet_ptr: [*c]const u8) callconv(.C) void {
+    _ = user;
+    _ = pkthdr;
+    _ = packet_ptr;
+    print("Yout just received a packet!\n", .{});
+}
+
 pub fn main() !void {
-    const device: *const [6]u8 = "wlp5s0";
-    var error_buf: [c.PCAP_ERRBUF_SIZE]u8 = undefined;
-    const capdev: ?*pcap_t = c.pcap_open_live(device, c.BUFSIZ, 0, -1, &error_buf);
+    var device: [*c]u8 = undefined;
+    var errbuf: [c.PCAP_ERRBUF_SIZE]u8 = undefined;
+    const packets: u32 = 10;
+
+    device = c.pcap_lookupdev(&errbuf);
+    const capdev: ?*pcap_t = c.pcap_open_live(device, c.BUFSIZ, 1, 0, &errbuf);
 
     if (capdev == null) {
-        print("ERR: pcap_openi_live() {s}\n", .{error_buf});
+        print("ERR: pcap_openi_live() {s}\n", .{errbuf});
+        return;
+    } else {
+        print("Device: {s}\n", .{device});
     }
+
+    const res = c.pcap_loop(capdev, packets, @ptrCast(&callback), null);
+    if (res < 0) {
+        print("ERR: pcap_loop() failed!\n", .{});
+        return;
+    }
+
+    c.pcap_close(capdev);
 }
